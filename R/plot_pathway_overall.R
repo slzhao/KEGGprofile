ae950c00f90754fece7a1159d90ecf498a3359d9
ShilinZhao <zhaoshilin@gmail.com>
2019-08-15 00:00:50 -0500

    Improvement for plot_pathway_overall function

--- R/plot_pathway_overall.R
+++ R/plot_pathway_overall.R
@@ -16,12 +16,11 @@
 ##' @return p values for Wilcoxon tests in each pathway
 ##' @examples data(pro_pho_expr)
 ##' data(pho_sites_count)
-##' genes<-row.names(pho_sites_count)[which(pho_sites_count>=10)]
-##' pho_KEGGresult<-find_enriched_pathway(genes,species='hsa',returned_genenumber=3)
-##' plot_pathway_overall(gene_values=pro_pho_expr[,1:3],kegg_enriched_pathway=pho_KEGGresult)
-plot_pathway_overall<-function(gene_values,kegg_enriched_pathway,groups=NULL,side=c("both","pos","neg"),alternative=NULL) {
+##' gene_values<-pro_pho_expr[row.names(pho_sites_count)[which(pho_sites_count>=10)],]
+##' plot_pathway_overall(gene_values=gene_values[,1:3])
+plot_pathway_overall<-function(gene_values,species="hsa",pathwayNumInFigure=5,rankByVar=colnames(gene_values)[1]) {
+  kegg_enriched_pathway=find_enriched_pathway(row.names(gene_values),species=species,returned_pvalue=1,returned_adjpvalue=1,returned_genenumber=5)
   names(kegg_enriched_pathway[[2]])=make.names(kegg_enriched_pathway[[1]]$Pathway_Name)
-  
   geneValuesInPathway<-lapply(kegg_enriched_pathway[[2]],function(x) gene_values[intersect(x,row.names(gene_values)),])
   
   geneValuesDiffPInPathway=sapply(geneValuesInPathway,function(x) {genesNotInPathway=setdiff(row.names(gene_values),row.names(x));pValue=rep(NA,ncol(x));for (i in 1:ncol(x)) {pValue[i]=wilcox.test(x[,i],gene_values[genesNotInPathway,i])$p.value};return(pValue)})
@@ -35,10 +34,14 @@ plot_pathway_overall<-function(gene_values,kegg_enriched_pathway,groups=NULL,sid
   dataForPlot=reshape2::melt(dataForPlot, id.vars="Pathway")
   
   #order by median value of first variable (column)
-  temp=dataForPlot[which(dataForPlot$variable==unique(dataForPlot$variable)[1]),]
-  pathwayOrder=tapply(temp$value,temp$Pathway,median)
+  temp=dataForPlot[which(dataForPlot$variable==rankByVar),]
+  pathwayOrder=tapply(temp$value,temp$Pathway,median,na.rm=T)
   pathwayOrder=names(pathwayOrder)[order(pathwayOrder)]
+  if (length(pathwayOrder)>pathwayNumInFigure*2) {
+    pathwayOrder=unique(c(head(pathwayOrder,pathwayNumInFigure),tail(pathwayOrder,pathwayNumInFigure)))
+  }
   
+  dataForPlot=dataForPlot[which(dataForPlot$Pathway %in% pathwayOrder),]
   dataForPlot$Pathway=factor(dataForPlot$Pathway,levels = pathwayOrder)
   
   p=ggplot(dataForPlot,aes(x=Pathway,y=value))+geom_boxplot(aes(colour=variable))
